d4df73944a143d3088cb71dd9cf45c8cb189409f
Alexandru Tudor <tudor.mihai.c7w@student.ucv.ro>
2024-11-05 20:12:08 +0200

    Vectors Solution

--- main.cpp
+++ main.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
+vector<pair<string, string>> problems, doctors;
+
+int main() {
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+
     inFile >> no_problems;
 
-    for (int i = 0; i < no_problems; i++)
-    {
+    for (int i = 0; i < no_problems; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        problems.push_back(make_pair(name, speciality));
     }
 
     inFile >> no_doctors;
 
-    for (int i = 0; i < no_doctors; i++)
-    {
+    for (int i = 0; i < no_doctors; i++) {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctors.push_back(make_pair(name, speciality));
+    }
+
+    for (auto& problem : problems) {
+        bool found = false;
+        for (auto& doctor : doctors)
+            if (problem.second == doctor.second) {
+                found = true;
+                break;
+            }
+
+        if (found)
+            cout << problem.first << ' ' << "Acceptat" << '\n';
+        else
+            cout << problem.first << ' ' << "Respins" << '\n';
     }
 
     return 0;
